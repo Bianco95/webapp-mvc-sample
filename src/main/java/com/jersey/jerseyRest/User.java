945a95e6919c3ccfd73e61ab95b9ac5dc7ad759d
giulio <giulio.bianchini@hotmail.it>
2020-12-05 11:04:40 +0100

    first commit

---
+++ src/main/java/com/jersey/jerseyRest/User.java
@@ -0,0 +1,52 @@
+package com.jersey.jerseyRest;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class User {
+	
+	private int amount;
+	private String firstName;
+	private String lastName;
+	private String address;
+	
+	/*
+	 * SETTERS
+	 */
+	
+	public void setFirstName(String name) {
+		this.firstName = name;
+	}
+	
+	public void setLastName(String lastname) {
+		this.lastName = lastname;
+	}
+	
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	/*
+	 * GETTERS
+	 */
+
+	public int getAmount() {
+		return this.amount;
+	}
+	
+	public String getFirstName() {
+		return this.firstName;
+	}
+	
+	public String getLastName() {
+		return this.lastName;
+	}
+	
+	public String getAddress() {
+		return this.address;
+	}
+}
---
+++ src/main/java/com/jersey/jerseyRest/UserRepository.java
@@ -0,0 +1,49 @@
+package com.jersey.jerseyRest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * @author giulio
+ * MOCK CLASS TO STORE DATA INSTEAD OF DB
+ */
+
+public class UserRepository {
+
+	private static UserRepository istance = null;
+	private List<User> users;
+
+	/*
+	 * CONSTRUCTOR
+	 */
+
+	public UserRepository() {
+		this.users = new ArrayList<User>();
+
+		User u1 = new User();
+		u1.setAmount(10);
+		u1.setFirstName("Giulio");
+		u1.setLastName("Bianchini");
+		u1.setAddress("test");
+
+		User u2 = new User();
+		u2.setAmount(10);
+		u2.setFirstName("Andrea");
+		u2.setLastName("Bianchini");
+		u2.setAddress("test");
+
+		this.users.add(u1);
+		this.users.add(u2);
+	}
+
+	public static UserRepository getIstance() {
+		if (istance == null)
+			istance = new UserRepository();
+		return istance;
+	}
+
+	public List<User> getUsers() {
+		return this.users;
+	}
+}

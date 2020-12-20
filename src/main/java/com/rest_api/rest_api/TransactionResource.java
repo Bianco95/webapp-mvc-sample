991be681d9c10adb2043d58435773ff9041c7cc3
giulio <giulio.bianchini@hotmail.it>
2020-12-20 22:52:11 +0100

    now only admin is able to create a transaction specifying customer id

--- src/main/java/com/rest_api/rest_api/TransactionResource.java
+++ src/main/java/com/rest_api/rest_api/TransactionResource.java
@@ -19,31 +19,31 @@ import com.google.gson.Gson;
 
 @Path("/transactions")
 public class TransactionResource extends AbstractResource {
-	
+
 	public TransactionResource() {
 		this.repository = TransactionRepository.getIstance();
 		this.resourceName = "transaction";
 	}
-	
+
 	@GET
 	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 	@Override
 	public void doGet(@Context HttpServletRequest request, @Context HttpServletResponse response)
 			throws IOException, ServletException {
-		System.out.println("[get"+this.resourceName+"] called...");
+		System.out.println("[get" + this.resourceName + "] called...");
 		try {
 			response.setContentType("json/html");
 			String username = request.getHeader("Authorization");
 			boolean isSuperAdmin = false;
-			System.out.println(username);
-			if(username == null) {
+			if (username == null) {
 				throw new ServletException("Auth header not provided");
 			}
 			isSuperAdmin = CustomerRepository.getIstance().isSuperAdmin(username);
 			this.dataSanitizing(request);
 			Integer gt = request.getParameter("gte") != null ? Integer.parseInt(request.getParameter("gte")) : null;
 			Integer lt = request.getParameter("lte") != null ? Integer.parseInt(request.getParameter("lte")) : null;
-			List<Transaction> transactions = ((TransactionRepository) this.repository).getTransactions(isSuperAdmin, username, gt, lt);
+			List<Transaction> transactions = ((TransactionRepository) this.repository).getTransactions(isSuperAdmin,
+					username, gt, lt);
 			this.sendAllContents(request, response, transactions);
 		} catch (Exception err) {
 			this.handleError(response, err);
@@ -59,6 +59,20 @@ public class TransactionResource extends AbstractResource {
 		try {
 			response.setContentType("json/html");
 			Transaction transaction = new Gson().fromJson(request.getReader(), Transaction.class);
+
+			String username = request.getHeader("Authorization");
+			if (username == null) {
+				throw new ServletException("Auth header not provided");
+			}
+			boolean isSuperAdmin = false;
+			isSuperAdmin = CustomerRepository.getIstance().isSuperAdmin(username);
+			int customerID = CustomerRepository.getIstance().getCustomerByUsername(username);
+			if (customerID == 1 && isSuperAdmin == false) {
+				throw new ServletException("not found");
+			}
+			if (!isSuperAdmin) {
+				transaction.setCustomerID(customerID);
+			}
 			int result = TransactionRepository.getIstance().createTransaction(transaction);
 			if (result != 1) {
 				throw new ServletException("not found");
@@ -81,14 +95,15 @@ public class TransactionResource extends AbstractResource {
 			if (transactionToUpdate == null) {
 				throw new ServletException("internal error");
 			}
-			Transaction oldTransaction = TransactionRepository.getIstance().getTransactionByTransactionID(transactionToUpdate.getTransactionID());
-			if(oldTransaction == null) {
+			Transaction oldTransaction = TransactionRepository.getIstance()
+					.getTransactionByTransactionID(transactionToUpdate.getTransactionID());
+			if (oldTransaction == null) {
 				throw new ServletException("not found");
 			}
 			TransactionRepository.getIstance().updateTransaction(transactionToUpdate);
 			this.sendPutResponse(request, response);
 		} catch (Exception err) {
-			if(err.getMessage() == "not found") {
+			if (err.getMessage() == "not found") {
 				response.sendError(HttpServletResponse.SC_NOT_FOUND);
 			} else {
 				response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
